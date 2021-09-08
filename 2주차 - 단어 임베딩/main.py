ab730f5e91fe93377fb32c871441831f070639f3
yehoon17 <yehoon17@gmail.com>
2021-09-08 11:13:01 +0900

    Rename file.py to main.py

---
+++ 2주차 - 단어 임베딩/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class WordVectorCorrelation:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    wv_corr = WordVectorCorrelation()
+    wv_corr.run()
