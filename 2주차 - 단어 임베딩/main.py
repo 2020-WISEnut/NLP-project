7a69e2597ebbcf6f3a416029998b22a9a32476d7
yehoon17 <yehoon17@gmail.com>
2021-09-09 17:04:00 +0900

    Update main.py
    Seperate testset loading.
    Remove dataset that those not include words in testset.

--- 2주차 - 단어 임베딩/main.py
+++ 2주차 - 단어 임베딩/main.py
@@ -26,7 +26,8 @@ class WordVectorCorrelation:
         save_path = config["save_path"]
         
         # load data and preprocess
-        datasets = self.load_data(load_path, preprocess_option)
+        testset, vocab = self.load_testset(load_path)
+        datasets = self.load_data(load_path, vocab, preprocess_option)
 
         # get word vectors
         word_vectors = self.get_word_vectors(datasets, vectorizer_type, model_arg)
@@ -37,7 +38,7 @@ class WordVectorCorrelation:
 
         # get lists of word similarities
         # 단어 유사도 리스트
-        answer_list, pred_list = self.get_word_similarity(word_vectors, load_path, vectorizer_type)
+        answer_list, pred_list = self.get_word_similarity(word_vectors, testset, vectorizer_type)
 
         # get spearman and pearson correlation
         spearman, pearson = self.get_correlation(answer_list, pred_list)
@@ -53,7 +54,24 @@ class WordVectorCorrelation:
 
         return config
 
-    def load_data(self, load_path, preprocess_option):
+    def load_testset(self, load_path):
+        testset_path = os.path.join(load_path, 'kor_ws353.csv')
+        testset = []
+        vocab = {}
+        try:
+            with open(testset_path, 'r', encoding='utf-8-sig') as testfile:
+                for pair in testfile:
+                    temp = pair.strip().split(',')
+                    testset.append(temp)   
+                    vocab.add(temp[0])   
+                    vocab.add(temp[1])
+        except IOError:
+            print("fail to open file")
+            sys.exit()
+
+        return testset, vocab
+
+    def load_data(self, load_path, vocab, preprocess_option):
         datasets = []
         # load data
         corpus_file_name = "wiki_ko_mecab.txt"
@@ -75,7 +93,8 @@ class WordVectorCorrelation:
             with open(os.path.join(load_path, corpus_file_name), 'r', encoding='utf-8') as f:
                 for line in f:
                     dataset = self.preprocess(line, preprocess_option, weird_words)
-                    datasets.append(dataset)
+                    if self.has_vocab(dataset, vocab):
+                        datasets.append(dataset)
         except IOError:
             print("failed to open file")
             sys.exit()
@@ -109,6 +128,13 @@ class WordVectorCorrelation:
         
         return text
 
+    def has_vocab(self, dataset: str, vocab: set):
+        for word in vocab:
+            if word in dataset:
+                return True
+
+        return False
+
     def get_word_vectors(self, datasets, vectorizer_type, model_arg):
         vectorizer = Vectorizer(vectorizer_type, model_arg)
         word_vectors = vectorizer.vectorize(datasets)
@@ -119,27 +145,28 @@ class WordVectorCorrelation:
         if vectorizer_type == 1:
             word_vectors.save_word2vec_format(os.path.join(save_path, file_name))
 
-    def get_word_similarity(self, word_vectors, load_path, vectorizer_type):
+    def get_word_similarity(self, word_vectors, testset, vectorizer_type):
         answer_list, pred_list = [], []
 
-        testset = 'kor_ws353.csv'
-        testset_path = os.path.join(load_path, testset)
-
         if vectorizer_type == 1:
-            try:
-                with open(testset_path, 'r', encoding='utf-8-sig') as testfile:
-                    for pair in testfile:
-                        w1, w2, sim = pair.strip().split(',')
-                        try:
-                            pred = word_vectors.similarity(w1, w2)
-                            answer_list.append(float(sim))
-                            pred_list.append(pred)
-                        except KeyError as e:
-                            # 단어 임베딩에 포함되지 않은 단어들
-                            print(e)
-            except IOError:
-                print("fail to open file")
-                sys.exit()
+            for pair in testset:
+                w1, w2, sim = pair.strip().split(',')
+                try:
+                    pred = word_vectors.similarity(w1, w2)
+                    answer_list.append(float(sim))
+                    pred_list.append(pred)
+                except KeyError as e:
+                    # 단어 임베딩에 포함되지 않은 단어들
+                    print(e)
+        elif self.vectorizer_type == 2:
+            ############ Fasttext ############
+            pass
+        elif self.vectorizer_type == 3:
+            ############# GloVe ##############
+            pass
+        elif self.vectorizer_type == 4:
+            ############# Swivel #############
+            pass
         else:
             print("vectorizer type not valid.")
             sys.exit()      
